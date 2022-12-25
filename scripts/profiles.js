fd5effd774e7dad4430b9e899d91b3a7edf6b22a
a25106 <a25106@svalero.com>
2022-12-25 18:45:36 +0100

    Added Create Profile

--- scripts/profiles.js
+++ scripts/profiles.js
@@ -5,13 +5,100 @@ let cookieId = document.cookie
 if (cookieId && cookieId != 0) {
     location.pathname = '/';
 }
-
+const defaultImageSource =
+    'https://bklyner.com/content/images/avatar/658c5875148fe3d738d2dd23da91fd3f.jpeg';
+let base64String = '';
 function showPopup(id) {
+    if (id) {
+        document.getElementById(
+            'popup-container'
+        ).innerHTML = `<h2>Introduce la contraseña</h2>
+                <form>
+                    <div class="password-field-input">
+                        <input id="passwordInput" type="password" oninvalid="this.classList.add('invalid')" placeholder="Contraseña"  required>
+                        <i id="passwordViewBut" class="bi bi-eye"></i>
+                    </div>
+                    <div class="buttons"><button class="button" onclick="hidePopup()">Cancelar</button><button class="button" id="login">Entrar</button>
+                </form>`;
+        document.querySelector('.popup button').id = 'submitbutton-' + id;
+    } else {
+        document.getElementById(
+            'popup-container'
+        ).innerHTML = `<h2>Crea un nuevo perfíl</h2>
+                <form>
+                    <div class="create-edit-profile-image">
+                        <img src="${defaultImageSource}" id="image-placeholder" onclick="document.getElementById('imageChanger').click()"
+                            alt="">
+                        <i class="bi bi-pencil"></i>
+                    </div>
+                    <input type="file" id="imageChanger" accept="image/*"  onchange="changeProfileImage()" style="display: none;">
+
+                    <div class="campos">
+                        <label for="usuario">Usuario</label>
+                        <input id="usuario" type="text" name="passnager-user" oninvalid="this.classList.add('invalid')"
+                            required>
+                    </div>
+                    <div class="campos">
+                        <label for="passwordInput">Contraseña</label>
+                        <div class="password-field-input">
+                            <input id="passwordInput" type="password" oninvalid="this.classList.add('invalid')"
+                                required>
+                            <i id="passwordViewBut" class="bi bi-eye"></i>
+                        </div>
+                    </div>
+
+                    <div class="buttons"><button class="button" onclick="hidePopup()">Cancelar</button><button class="button" id="create">Crear</button>
+                    </div>
+
+                </form>`;
+    }
+    document.getElementById('passwordViewBut').addEventListener('click', () => {
+        viewPassword();
+    });
     document.getElementById('popup').classList.add('popup-visible');
-    document.querySelector('.popup button').id = 'submitbutton-' + id;
 }
 function hidePopup() {
     document.getElementById('popup').classList.remove('popup-visible');
+}
+
+function changeProfileImage() {
+    var file = document.getElementById('imageChanger')['files'][0];
+    if (file.size > 100000000) {
+        alert('EL ARCHIVO ES DE MÁS DE UN GB, ESTÁS BIEN?');
+        file.value = '';
+        return;
+    }
+    var reader = new FileReader();
+
+    reader.onload = function () {
+        base64String = reader.result.replace('data:', '').replace(/^.+,/, '');
+        imageBase64Stringsep = base64String;
+        document.getElementById('image-placeholder').src =
+            'data:image/png;base64, ' + base64String;
+    };
+
+    reader.readAsDataURL(file);
+}
+
+async function createProfile() {
+    let icon = document.getElementById('image-placeholder').src;
+    let res = await fetch('https://localhost:7027/Profile', {
+        method: 'POST',
+        headers: {
+            Accept: '*/*',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            icon: icon === defaultImageSource ? null : base64String,
+            name: document.getElementById('usuario').value.trim(),
+            password: document.getElementById('passwordInput').value.trim(),
+        }),
+    });
+    res.json().then((data) => {
+        console.log(data);
+        if (!data) return;
+        location.reload();
+    });
 }
 
 async function login() {
@@ -29,6 +116,7 @@ async function login() {
         }),
     });
     res.json().then((data) => {
+        console.log(data);
         if (!data) return;
         document.cookie = 'id=' + id + '; Secure; path=/';
         location.pathname = '/';
@@ -59,9 +147,9 @@ async function drawRows() {
         row.classList.add('row');
         row.innerHTML = `
         <div class="icon-container"><img src="${
-            profile.icon || profile.icon.length > 1
+            profile.icon?.length > 1
                 ? 'data:image/png;base64,' + profile.icon
-                : 'https://bklyner.com/content/images/avatar/658c5875148fe3d738d2dd23da91fd3f.jpeg'
+                : defaultImageSource
         }" alt=""></div>
         <h3>${profile.name}</h3>
         <button class="button" onclick="showPopup(${
@@ -74,15 +162,14 @@ async function drawRows() {
 
 document.addEventListener('submit', (e) => {
     e.preventDefault();
-    login();
+    if (e.submitter.id === 'login') login();
+    if (e.submitter.id === 'create') createProfile();
 });
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('popup').addEventListener('click', (e) => {
         if (e.target.id === 'popup') hidePopup();
     });
-    document.getElementById('passwordViewBut').addEventListener('click', () => {
-        viewPassword();
-    });
+
     drawRows();
 });
