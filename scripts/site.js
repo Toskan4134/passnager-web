35677d39cfcee2f1b1cd662b99d9983ba52f961f
a25106 <a25106@svalero.com>
2022-12-26 03:02:34 +0100

    CreateSite and EditSite added

--- scripts/site.js
+++ scripts/site.js
@@ -1,3 +1,11 @@
+let cookieId = document.cookie
+    .split('; ')
+    .find((row) => row.startsWith('id='))
+    ?.split('=')[1];
+if (!cookieId || cookieId == 0) {
+    location.pathname = '/pages/profiles.html';
+}
+let siteId;
 function generatePassword(min) {
     const characters =
         'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
@@ -23,10 +31,126 @@ function viewPassword() {
     }
 }
 
+async function generateSite() {
+    let url = new URL(location.href);
+    let cats;
+    let categories = document.getElementById('categories');
+    let submitButton = document.getElementById('submitButton');
+    switch (url.searchParams.get('mode')) {
+        case 'create':
+            submitButton.id = 'create';
+            cats = await (
+                await fetch('https://localhost:7027/Category/' + cookieId)
+            ).json();
+            cats.forEach((c) => {
+                let child = document.createElement('option');
+                child.value = c.id;
+                child.innerText = c.name;
+                if (c.id == url.searchParams.get('id')) child.selected = true;
+                categories.appendChild(child);
+            });
+            break;
+        case 'edit':
+            submitButton.value = 'Editar';
+            submitButton.id = 'edit';
+            document.getElementById('title').innerText = 'Editar Sitio';
+            let res = await fetch(
+                'https://localhost:7027/Site/' + url.searchParams.get('id')
+            );
+            let data = await res.json();
+            siteId = data.id;
+            document.getElementById('sitio').value = data.site;
+            document.getElementById('usuario').value = data.user;
+            document.getElementById('url').value = data.url;
+            document.getElementById('passwordInput').value = data.password;
+            document.getElementById('descripción').value = data.description;
+            cats = await (
+                await fetch('https://localhost:7027/Category/' + cookieId)
+            ).json();
+            cats.forEach((c, i) => {
+                let child = document.createElement('option');
+                if (c.id === data.categoryId) child.selected = true;
+                child.value = c.id;
+                child.innerText = c.name;
+                categories.appendChild(child);
+            });
+            break;
+        default:
+            location.pathname = '/';
+            break;
+    }
+}
+
+async function editSite() {
+    let res = await fetch('https://localhost:7027/Site', {
+        method: 'PUT',
+        headers: {
+            Accept: '*/*',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            id: siteId,
+            categoryId: document.getElementById('categories').value,
+            site: document.getElementById('sitio').value.trim(),
+            url: document.getElementById('url').value.trim(),
+            user: document.getElementById('usuario').value.trim(),
+            password: document.getElementById('passwordInput').value.trim(),
+            description: document.getElementById('descripción').value,
+        }),
+    });
+    await res.json().then(async () => {
+        let url = new URL(location.href);
+        url.pathname = '/';
+        let params = Array.from(url.searchParams);
+        for (const param in params) {
+            url.searchParams.delete(params[param][0]);
+            if (param == params.length - 1) location.href = url;
+        }
+    });
+}
+
+function closeSite() {
+    let url = new URL(location.href);
+    url.pathname = '/';
+    let params = Array.from(url.searchParams);
+    for (const param in params) {
+        url.searchParams.delete(params[param][0]);
+        if (param == params.length - 1) location.href = url;
+    }
+}
+
+async function createSite() {
+    let res = await fetch('https://localhost:7027/Site', {
+        method: 'POST',
+        headers: {
+            Accept: '*/*',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            categoryId: document.getElementById('categories').value,
+            site: document.getElementById('sitio').value.trim(),
+            url: document.getElementById('url').value.trim(),
+            user: document.getElementById('usuario').value.trim(),
+            password: document.getElementById('passwordInput').value.trim(),
+            description: document.getElementById('descripción').value,
+        }),
+    });
+    await res.json().then(async () => {
+        let url = new URL(location.href);
+        url.pathname = '/';
+        let params = Array.from(url.searchParams);
+        for (const param in params) {
+            url.searchParams.delete(params[param][0]);
+            if (param == params.length - 1) location.href = url;
+        }
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const genPasswordBut = document.getElementById('genPasswordBut');
     const passwordInput = document.getElementById('passwordInput');
     const passwordViewBut = document.getElementById('passwordViewBut');
+    generateSite();
 
     genPasswordBut.addEventListener('click', () => {
         const password = generatePassword(16);
@@ -36,4 +160,10 @@ document.addEventListener('DOMContentLoaded', () => {
     passwordViewBut.addEventListener('click', () => {
         viewPassword();
     });
+});
+
+document.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (e.submitter.id === 'create') createSite();
+    if (e.submitter.id === 'edit') editSite();
 });
