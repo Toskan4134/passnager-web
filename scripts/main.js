b4e1c505b08ed3148fcee9e9deaeb00698ed151c
a25106 <a25106@svalero.com>
2022-12-27 23:10:37 +0100

    Added CreateCategory

--- scripts/main.js
+++ scripts/main.js
@@ -6,9 +6,11 @@ if (!cookieId || cookieId == 0) {
     location.pathname = '/pages/profiles.html';
 }
 let selectedCategoryId;
-
+let base64String;
 let searchButton = document.getElementById('searchButton');
 let searchInput = document.getElementById('searchInput');
+const defaultImageSource =
+    'https://cdn.iconscout.com/icon/free/png-256/category-2456577-2036097.png';
 
 async function drawCategories() {
     let parent = document.getElementById('categories');
@@ -32,7 +34,7 @@ async function drawCategories() {
             categoryElement.innerHTML = `<div class="icon-container"><img src="${
                 category.icon?.length > 1
                     ? 'data:image/png;base64,' + category.icon
-                    : 'https://cdn.iconscout.com/icon/free/png-256/category-2456577-2036097.png'
+                    : defaultImageSource
             }"></div><h3>${category.name}</h3>`;
             categoryElement.addEventListener('click', () => {
                 drawSites(category.id);
@@ -114,12 +116,93 @@ function goToSite(mode = 'create', id = selectedCategoryId) {
     location.href = url;
 }
 
+function showPopup() {
+    document.getElementById('popup-container').innerHTML = `
+            <h2>Crear Categoría</h2>
+            <form>
+                <div class="create-edit-profile-image">
+                    <img src="${defaultImageSource}"
+                        id="image-placeholder" onclick="document.getElementById('imageChanger').click()" alt="">
+                    <i class="bi bi-pencil"></i>
+                </div>
+                <button class="button" type="button" onclick="resetImage()">Reestablecer Imágen</button>
+                <input type="file" id="imageChanger" accept="image/*" onchange="changeProfileImage()"
+                    style="display: none;">
+
+                <div class="campos">
+                    <label for="categoría">Nombre de Categoría</label>
+                    <input id="categoría" type="text" name="passnager-category" oninvalid="this.classList.add('invalid')"
+                        required>
+                </div>
+                <div class="buttons"><button class="button" type="button" onclick="hidePopup()">Cancelar</button>
+                    <button class="button" id="create">Crear</button>
+                </div>
+
+            </form>`;
+    document.getElementById('popup').classList.add('popup-visible');
+}
+function hidePopup() {
+    document.getElementById('popup').classList.remove('popup-visible');
+    document.getElementById('accept-popup').classList.remove('popup-visible');
+}
+function resetImage() {
+    document.getElementById('image-placeholder').src = defaultImageSource;
+}
+
+function changeProfileImage() {
+    var file = document.getElementById('imageChanger')['files'][0];
+    if (file.size > 100000000) {
+        alert('EL ARCHIVO ES DE MÁS DE UN GB, ESTÁS BIEN?');
+        file.value = '';
+        return;
+    }
+    var reader = new FileReader();
+
+    reader.onload = function () {
+        base64String = reader.result.replace('data:', '').replace(/^.+,/, '');
+        imageBase64Stringsep = base64String;
+        document.getElementById('image-placeholder').src =
+            'data:image/png;base64, ' + base64String;
+    };
+
+    reader.readAsDataURL(file);
+}
+
+async function createCategory() {
+    let icon = document.getElementById('image-placeholder').src;
+    let res = await fetch('https://localhost:7027/Category', {
+        method: 'POST',
+        headers: {
+            Accept: '*/*',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            profileId: cookieId,
+            icon: icon === defaultImageSource ? null : base64String,
+            name: document.getElementById('categoría').value.trim(),
+        }),
+    });
+    res.json().then((data) => {
+        if (!data) return;
+        location.reload();
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     drawCategories().then(() => {
         let firstCategory = document.querySelectorAll('#categories li').item(0);
         firstCategory.click();
         selectedCategoryId = firstCategory.id.split('-')[1];
     });
+    document.getElementById('popup').addEventListener('click', (e) => {
+        if (e.target.id === 'popup') hidePopup();
+    });
+});
+
+document.addEventListener('submit', (e) => {
+    if (e.submitter.id === 'create') {
+        createCategory();
+    }
 });
 
 searchInput.addEventListener('input', () => {
